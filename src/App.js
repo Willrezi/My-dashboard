590f660eedaf31128ae575f870ffd74087d92f7b
Wilfried LLOZA <wilfried.lloza@gmail.com>
2019-03-19 12:08:40 +0100

    route creation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React, { Component, Fragment } from "react";
-/* import "./style.css"; */
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+
+import "./App.css";
+
+import Header from "./components/Header";
+import SignUp from "./containers/SignUp";
+import LogIn from "./containers/LogIn";
+import Home from "./containers/Home";
 
 class App extends Component {
   render() {
-    return <Fragment>This is the App component</Fragment>;
+    const withLayout = Component => {
+      return (
+        <div>
+          <Header />
+          <Component />
+        </div>
+      );
+    };
+    return (
+      <Router>
+        <Fragment>
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/login" />
+            </Route>
+            <Route path="/signup" component={SignUp} />
+            <Route path="/login" component={LogIn} />
+            <Route path="/home" render={() => withLayout(Home)} />
+          </Switch>
+        </Fragment>
+      </Router>
+    );
   }
 }
 
