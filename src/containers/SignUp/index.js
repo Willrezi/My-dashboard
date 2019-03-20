9b888c48efae1ec5a002dead8e640f4c96d5fc93
Wilfried LLOZA <wilfried.lloza@gmail.com>
2019-03-20 15:40:31 +0100

    signup page working

--- src/containers/SignUp/index.js
+++ src/containers/SignUp/index.js
@@ -1,4 +1,7 @@
 import React, { Component, Fragment } from "react";
+import Cookies from "js-cookie";
+import axios from "axios";
+import swal from "sweetalert";
 
 import "./style.css";
 import sign from "../../assets/images/sign.png";
@@ -12,6 +15,73 @@ class SignUp extends Component {
     errorMessage: ""
   };
 
+  onSubmit = event => {
+    const { email, username, password, confirmedPassword } = this.state;
+    if (email === "") {
+      swal({
+        title: "Veuillez saisir votre email",
+        icon: "warning",
+        dangerMode: true
+      });
+    } else if (username === "") {
+      swal({
+        title: "Veuillez saisir votre Pseudo",
+        icon: "warning",
+        dangerMode: true
+      });
+    } else if (password === "") {
+      swal({
+        title: "Veuillez saisir votre mot de passe",
+        icon: "warning",
+        dangerMode: true
+      });
+    } else if (confirmedPassword === "") {
+      swal({
+        title: "Veuillez confirmer votre mot de passe",
+        icon: "warning",
+        dangerMode: true
+      });
+    }
+    if (password !== confirmedPassword) {
+      this.setState({
+        errorMessage: "Les mots de passe ne sont pas identiques"
+      });
+    } else {
+      axios
+        .post("http://localhost:3100/api/user/sign_up", {
+          email: email,
+          username: username,
+          password: password
+        })
+        .then(response => {
+          console.log(response.data);
+          if (response.data.token) {
+            Cookies.set("token", response.data.token);
+            Cookies.set("username", response.data.username);
+            Cookies.set("_id", response.data._id);
+          }
+          this.props.history.push("/home");
+        })
+        .catch(error => {
+          console.log(error.message);
+        });
+    }
+    // } else {
+    //   this.setState({
+    //     errorMessage: "Les mots de passe ne sont pas identiques"
+    //   });
+    // }
+    event.preventDefault();
+  };
+
+  renderError() {
+    if (this.state.errorMessage) {
+      return <p>{this.state.errorMessage}</p>;
+    } else {
+      return null;
+    }
+  }
+
   render() {
     return (
       <Fragment>
@@ -22,25 +92,54 @@ class SignUp extends Component {
           <div className="signup-content">
             <h1>Bienvenue sur votre Dashboard</h1>
             <h2>Créer un compte</h2>
-            <form>
+            <form onSubmit={this.onSubmit}>
               <div className="signup-input">
                 <label>Adresse email</label>
-                <input type="email" />
+                <input
+                  type="email"
+                  onChange={event => {
+                    this.setState({ email: event.target.value });
+                  }}
+                  value={this.state.email || ""}
+                />
               </div>
               <div className="signup-input">
                 <label>Pseudo</label>
-                <input type="text" />
+                <input
+                  type="text"
+                  onChange={event => {
+                    this.setState({ username: event.target.value });
+                  }}
+                  value={this.state.username || ""}
+                />
               </div>
               <div className="signup-password">
                 <div className="password-left">
                   <label htmlFor="password">Mot de passe</label>
-                  <input id="password" name="password" type="password" />
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    onChange={event => {
+                      this.setState({ password: event.target.value });
+                    }}
+                    value={this.state.password}
+                  />
                 </div>
                 <div className="password-right">
                   <label htmlFor="password">Confirmer le mot de passe</label>
-                  <input id="password" name="password" type="password" />
+                  <input
+                    id="confirmedPassword"
+                    name="confirmedPassword"
+                    type="password"
+                    onChange={event => {
+                      this.setState({ confirmedPassword: event.target.value });
+                    }}
+                    value={this.state.confirmedPassword}
+                  />
                 </div>
               </div>
+              <div className="error-message">{this.renderError()}</div>
               <button className="signup-button" type="submit">
                 Inscription
               </button>
